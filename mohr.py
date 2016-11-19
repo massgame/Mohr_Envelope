a5f9fdb2893284a9bf583ab424ec2fed6b90d1fa
Kim SK <soyuz67@gmail.com>
2016-11-19 21:51:22 +0900

    Update mohr.py
    it is now very object oriented

--- mohr.py
+++ mohr.py
@@ -1,60 +1,83 @@
-
 from scipy import stats
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
-import sys
+import csv,sys
 
-st1=[] #Minor stress
-st2=[] #Major stress
 
-with open(sys.argv[1], newline='') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		st1.append(float(row[1])) #Reading Confining Stress
-		st2.append(float(row[2])) #Reading Axial Stress
+
+
+class Read():
+	def __init__(self, promptFileName):
+		self.fileName = promptFileName
+	
+	def readCsv(self):
+		stress_Minor = [] #Minor stress
+		stress_Major = [] #Major stress
+		with open(self.fileName, newline='') as f:
+			reader = csv.reader(f)
+			for row in reader:
+				stress_Minor.append(float(row[1])) #Reading Confining Stress
+				stress_Major.append(float(row[2])) #Reading Axial Stress
+		circleCenter = [(stress_Major[i]+stress_Minor[i])/2 for i in range(len(stress_Minor))] #center of Mohr Circle 
+		circleRadius = [(stress_Major[i]-stress_Minor[i])/2 for i in range(len(stress_Minor))] #radius of Mohr Circle
+		return stress_Minor, stress_Major, circleCenter, circleRadius
+	
+class getEnvelope():
+	def __init__(self):
+		self.scaleX = np.linspace(1, 90, 9000, endpoint=True) #0.01 scale
+	
+	def getlineParam(self, angle, center, radius):
+		meetPointX = [center[i]-(radius[i]*np.cos(np.deg2rad(angle))) for i in range(len(center))] #tangent line - circle meet points x
+		meetPointY = [radius[i]*np.sin(np.deg2rad(angle)) for i in range(len(center))]#tangent line - circle meet points y
+		slope, intercept, r_value, p_value, std_err = stats.linregress(meetPointX, meetPointY) #get line by least square method
+		return slope, intercept, meetPointX, meetPointY
 		
-circlecenter=[(st2[i]+st1[i])/2 for i in range(len(st1))] #center of Mohr Circle 
-circleradius=[(st2[i]-st1[i])/2 for i in range(len(st1))] #radius of Mohr Circle
-X = np.linspace(1, 90, 9000, endpoint=True) #0.01 scale
-Avgs=[] #save average differences
+	def getreallineParam(self, _center, _radius):
+		Avgs=[] #save average differences
+		f=open("test.txt","w")
+		for k in range(len(self.scaleX)):
+			_slope, _intercept, _mx, _my = self.getlineParam(self.scaleX[k], _center, _radius)
+			Avgs.append(np.mean([_radius[i]-np.absolute(_slope*_center[i]+_intercept)/np.sqrt(_slope**2+1) for i in range(len(_radius))])) # add calculated average in list Avgs
+		return round(self.scaleX[Avgs.index((min(Avgs)))],2)
+		
 
-f=open("output.txt","w")
-for k in range(len(X)):
-	degreex=[circlecenter[i]-(circleradius[i]*np.cos(np.deg2rad(X[k]))) for i in range(len(circlecenter))] #tangent line - circle meet points x
-	degreey=[circleradius[i]*np.sin(np.deg2rad(X[k])) for i in range(len(circlecenter))]#tangent line - circle meet points y
-	slope, intercept, r_value, p_value, std_err=stats.linregress(degreex,degreey) #get line by least square method
-	Distances=[(np.absolute(slope*circlecenter[i]+intercept)/np.sqrt(slope**2+1)) for i in range(len(degreex))] #get distance between line and center point of circle
-	Avgs.append(np.mean([circleradius[i]-Distances[i] for i in range(len(circleradius))])) # add calculated average in list Avgs
+
+class Visualize():
+	def __init__(self):
+		self.fig, self.ax = plt.subplots() 
+		self.ax.axis('scaled')
+		self.ax.axhline(0, color='black')
+		self.ax.axvline(0, color='black')
+		self.ax.set(xlabel=r'Normal Stress(MPa)', ylabel=r'Shear Stress(MPa)')
+		
+	def drawCircle(self, _radius_, _center_, _stress_Major):
+		self.ax.set_xlim((-10,_stress_Major[-1]+200)) 
+		self.ax.set_ylim((-_radius_[-1]-200,_radius_[-1]+200))
+		for j in range(len(_stress_Major)):
+			self.ax.add_artist(plt.Circle((_center_[j], 0), _radius_[j],edgecolor='red',label='Mohr Circle',fill=False)) #draw mohr circle
 	
-degr=round(X[Avgs.index((min(Avgs)))],2) #get a degree which has lowest differences
-degreex=[circlecenter[i]-(circleradius[i]*np.cos(np.deg2rad(degr))) for i in range(len(circlecenter))] #tangent line - circle meet points x
-degreey=[circleradius[i]*np.sin(np.deg2rad(degr)) for i in range(len(circlecenter))] #tangent line - circle meet points y
-slope, intercept, r_value, p_value, std_err=stats.linregress(degreex,degreey) #get line by least square method
+	def drawEnvelope(self, pointX, pointY, _slope, _intercept, _stress_Minor_, center, _degr):
+		envelope = self.ax.plot(pointX+[1000],[(pointX[i]*_slope)+_intercept for i in range(len(pointX))]+[1000*_slope+_intercept],label='Failure Envelope')
+		lineRadius = self.ax.plot([pointX[-1],center[-1]],[pointY[-1],0],label="Radius of Test {0} Mohr Circle".format(len(_stress_Minor_)))
+		self.ax.text(center[-1],0, '%.2f° '%(_degr), fontsize=12) #theta angle
+		self.ax.legend(fontsize=10)
+		if _intercept<0: 
+			self.ax.text(pointX[-1],pointY[-1], r'$y=%.2fx%.2f$'%(_slope,_intercept), fontsize=12)#equation of failure envelope
+		else:
+			self.ax.text(pointX[-1],pointY[-1], r'$y=%.2fx+%.2f$'%(_slope,_intercept), fontsize=12)
+		
+	def writepngFile(self):
+		self.fig.savefig('Fig.png')
+		
+
+csvReader=Read(sys.argv[1])
+sm, smj, cc, cr = csvReader.readCsv()		
+env = getEnvelope()
+ag = env.getreallineParam(cc, cr)
+slp, icpt, mx, my = env.getlineParam(ag, cc, cr)
+graph = Visualize()
+graph.drawCircle(cr, cc, smj)
+graph.drawEnvelope(mx, my, slp, icpt, sm, cc, ag)
+graph.writepngFile()
 
 
-
-fig, ax = plt.subplots() 
-ax.axis('scaled')
-ax.axhline(0, color='black')
-ax.axvline(0, color='black')
-ax.set(xlabel=r'Normal Stress(MPa)', ylabel=r'Shear Stress(MPa)')
-ax.set_xlim((-10,st2[-1]+200)) 
-ax.set_ylim((-circleradius[-1]-200,circleradius[-1]+200))
-
-for j in range(len(st1)):
-	ax.add_artist(plt.Circle((circlecenter[j], 0), circleradius[j],edgecolor='red',label='Mohr Circle',fill=False)) #draw mohr circle
-
-linet=ax.plot(degreex+[1000],[(degreex[i]*slope)+intercept for i in range(len(degreex))]+[1000*slope+intercept],label='Failure Envelope')
-linef=ax.plot([degreex[-1],circlecenter[-1]],[degreey[-1],0],label="Radius of Test {0} Mohr Circle".format(len(st1)))
-ax.legend(fontsize=10)
-if intercept<0: 
-	ax.text(degreex[-1],degreey[-1], r'$y=%.2fx%.2f$'%(slope,intercept), fontsize=12)
-	f.write("===============\n Internal Friction Angle : {0:.2f}° \n Cohesion : {1:.2f} MPa \n Failure Envelope equation : y={2:.2f}x{1:.2f}\n===============".format(90-degr,intercept,slope))
-else:
-	ax.text(degreex[-1],degreey[-1], r'$y=%.2fx+%.2f$'%(slope,intercept), fontsize=12)
-	f.write("===============\n Internal Friction Angle : {0:.2f}° \n Cohesion : {1:.2f} MPa \n Failure Envelope equation : y={2:.2f}x+{1:.2f}\n===============".format(90-degr,intercept,slope))
-	
-f.close()
-ax.text(circlecenter[-1],0, '%.2f° '%(degr), fontsize=12) #equation of failure envelope
-fig.savefig('Fig.png')
